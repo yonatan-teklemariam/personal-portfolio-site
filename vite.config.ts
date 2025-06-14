99ac07cbc00dcd67248b1849acf6ba4b10f30a8d
yonatan-teklemariam <ys20210613@gmail.com>
2025-06-14 18:42:54 +0100

    Update vite.config.ts
    Signed-off-by: yonatan-teklemariam <ys20210613@gmail.com>

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
-import { componentTagger } from "lovable-tagger";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-  },
-  base: "/personal-portfolio/",
-  plugins: [
-    react(),
-    mode === 'development' &&
-    componentTagger(),
-  ].filter(Boolean),
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-}));
+export default defineConfig({
+  base: '/personal-portfolio/', // 👈 IMPORTANT: Replace with your actual repo name
+  plugins: [react()],
+})
